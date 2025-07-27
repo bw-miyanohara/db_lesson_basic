6ecbb6754e6aae3775f5429f3bd801e22d3762a1
{bw-miyanohara} <{kmiyanohara730@gmail.com}>
2025-07-27 14:33:36 +0900

    宿題に回答

--- db_lesson.sql
+++ db_lesson.sql
@@ -7,7 +7,10 @@ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-Q2.
+
+SELECT * FROM departments;
+
+Q2.⭐️
 
 ALTER TABLE people ADD department_id INT UNSIGNED AFTER email; 
 
@@ -34,6 +37,8 @@ VALUES
 ('敷島', 4),
 ('常田', 5);
 
+
+⭐️
 INSERT INTO reports (person_id, content)
 VALUES
 (7, '人生で一番だらだらできる時間にだらだらできる帰宅部が一番だ！'),
@@ -45,7 +50,7 @@ VALUES
 (13, 'その映画は感動的で涙が止まりませんでした。'),
 (14, '道に迷ってしまって少し遅れてしまいました。'),
 
-Q4.
+Q4.⭐️
 
 SELECT * FROM people WHERE department_id IS NULL;
 
@@ -72,14 +77,29 @@ Q8.
 
 SELECT * FROM people WHERE department_id = 1 ORDER BY age ASC;
 
-Q9.
+Q9.⭐️
 
 SELECT AVG(age) AS average_age FROM people WHERE department_id = 2;
 
-Q10.
+Q10.⭐️
 
 SELECT d.name, p.name, r.content FROM people AS p RIGHT OUTER JOIN reports AS r ON p.person_id = r.person_id RIGHT OUTER JOIN departments AS d ON p.department_id = d.department_id;
 
-Q11.
+Q11.⭐️
 
 SELECT p.person_id, p.name FROM people p LEFT OUTER JOIN reports r USING (person_id) WHERE content IS NULL;
+
+
+-宿題の回答-
+
+Q1.なぜPRIMARY KEYを使用するのか
+→レコードを特定するための値であり、データの重複を許さないためデータの整合性が保たれるから。
+
+Q4.person_idが4,5,6のdepartment_idを1に一つのクエリでまとめる
+→UPDATE people SET department_id = 1 WHERE person_id IN (4,5,6);
+
+Q10.クエリの改善・外部結合から内部結合を使用してデータを取得する
+→SELECT d.name, p.name, r.content FROM people AS p INNER JOIN reports AS r ON p.person_id = r.person_id INNER JOIN departments AS d ON p.department_id = d.department_id;
+
+Q11.ONとUSINGの違いを調べる
+→USINGは同じカラム名を持つテーブルを完結で可読性が高く結合でき、ONは任意の条件で異なるカラムや複数条件で結合が可能。
